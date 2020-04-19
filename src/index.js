008007cbcd0e475fcbd90ce948fa14b6a8df96a2
ikumi.otani <ikumi.otani@kaonavi.jp>
2020-04-20 02:05:30 +0900

    Reactのデフォルトプロジェクト画面を表示

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App.js'
 
+// もともとのコード
+//ReactDOM.render(
+//  <h1>Hello, React! my hands on </h1>,
+//  document.getElementById('root')
+//);
+//　新しいコード
+const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <h1>Hello, React! my hands on </h1>,
-  document.getElementById('root')
+  <>
+  <h1>Hello, React! my hands on </h1>
+  <App />
+  </>,
+  rootElement
 );
